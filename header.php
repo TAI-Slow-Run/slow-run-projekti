00dfc1beaa9b184763d9fc908d40e2d3e7bc29d5
SKSTheo93 <athrikardo@gmail.com>
2025-09-07 19:33:33 +0300

    Fixed the burger menu. Transition of .3s from right to left. Same background color, text color and font color as the banner

--- header.php
+++ header.php
@@ -8,6 +8,14 @@
         <li><a href="./contact.php" class="nav-link">YHTEYS</a></li>
       </ul>
     </nav>
+    <!--**************************************************** -->
+    <div class = "off-screen-menu"> <!-- Added by Riku -->
+      <ul>
+        <li><a href="./news.php"    class="nav-link">UUTISET</a></li>
+        <li><a href="./contact.php" class="nav-link">YHTEYS</a></li>
+      </ul>
+    </div>
+    <!-- *******DIV element added by Riku ends here ********-->
     <i class="uil uil-bars hamburger" id="hamburger"></i>
   </div>
 </header>
