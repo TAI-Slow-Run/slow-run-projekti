d0467c064dc3c447babbf716d1b5e80537954798
SKSTheo93 <athrikardo@gmail.com>
2025-09-11 16:50:29 +0300

    Fixed login page.

--- js_php/login.js
+++ js_php/login.js
@@ -3,6 +3,8 @@ import * as dUtils from "../databaseTools/databaseUtilities.js";
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("submitButton").addEventListener("click", async (event) => {
         const data = await dUtils.sendDataToServer(event, "./js_php/login_server.php", null);
-        console.log(data);
+        if(data.keyData == "ok") {
+            dUtils.redirect("welcome.php", null);
+        }
     });
 });
