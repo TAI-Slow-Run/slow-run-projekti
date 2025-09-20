f7e42f9109073ce34cdea3f1519cea624ee815aa
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-09-21 00:06:57 +0300

    Upload of the new article to the DB and a new article-image to the server is ready

--- js_php/admin-menu-add-article.js
+++ js_php/admin-menu-add-article.js
@@ -1,37 +1,40 @@
-// import * as dUtils from "../databaseTools/databaseUtilities.js";
+import * as dUtils from "../databaseTools/databaseUtilities.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("return-btn").addEventListener("click", async (event) => {
+            dUtils.redirect("admin-menu.php", null);
+    });
+});
 
 function checkFileType() {
+  let fileElement = document.getElementById("img-file");
+  let fileToCheck = "";
+  if ("files" in fileElement) {
+    fileToCheck = fileElement.files[0]["type"];
+  }
+  if (fileToCheck !== "image/jpeg") {
+    handleErrorPageChangeTheTypeOfTheFile();
+  }
+}
 
-    let fileElement = document.getElementById("img-file");
-    console.log("Files are: ", fileElement.files[0]["type"]);
-    let fileToCheck = "";
-    if ("files" in fileElement) {
-        
-      fileToCheck = fileElement.files[0]["type"];
-    }
-    if (fileToCheck !=="image/jpeg" && fileToCheck !=="image/png") {
-      handleErrorPageChangeTheTypeOfTheFile();
-    }
-  }
-  
-  function handleErrorPageChangeTheTypeOfTheFile() {
-    alert("Valitse toinen tiedostotyyppi. Sen tulisi olla .jpg tai .png.");
-    document.getElementById("img-file").value = "";
-  }
+function handleErrorPageChangeTheTypeOfTheFile() {
+  alert("Valitse toinen tiedostotyyppi. Sen tulisi olla VAIN .jpg.");
+  document.getElementById("img-file").value = "";
+}
 
-  function validateForm(event) {
-    const inputTitle = document.getElementById("article-title");
-    const inputText = document.getElementById("article-text");
-    const inputFile = document.getElementById("img-file");
-  
-    if (inputTitle.value.trim() === "") {
-      alert("Kirjoita artikkelin otsikko!");
-      event.preventDefault();
-    } else if (inputText.value.trim() === "") {
-        alert("Kirjoita artikkelin teksti!");
-      event.preventDefault();
-    } else if (inputFile.value.trim() === "") {
-      alert("Lataa tiedosto!");
-      event.preventDefault();
-    }
+function validateForm(event) {
+  const inputTitle = document.getElementById("article-title");
+  const inputText = document.getElementById("article-text");
+  const inputFile = document.getElementById("img-file");
+
+  if (inputTitle.value.trim() === "") {
+    alert("Kirjoita artikkelin otsikko!");
+    event.preventDefault();
+  } else if (inputText.value.trim() === "") {
+    alert("Kirjoita artikkelin teksti!");
+    event.preventDefault();
+  } else if (inputFile.value.trim() === "") {
+    alert("Lataa tiedosto!");
+    event.preventDefault();
   }
+}
