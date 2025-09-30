b36d3405836ec7852430b02eed8e22ec5927fcbc
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-09-30 15:58:47 +0300

    Admin Page Change main image is ready. Next step: upload it to the server

--- admin-menu-change-hero-img.php
+++ admin-menu-change-hero-img.php
@@ -6,7 +6,7 @@ include("./databaseTools/connectionLibrary.php");
 include("./databaseTools/validationUtilities.php");
 
 $admin_id = validate_session($connection);
-if(!$admin_id) {
+if (!$admin_id) {
     header("Location: login.php");
     exit();
 }
@@ -35,17 +35,41 @@ if(!$admin_id) {
     <?php include __DIR__ . '/header.php'; ?>
     <button type="submit" id="return-btn" class="admin-btn top-admin-btn">Palaa toimintoon valitsemalla</button>
 
-    <!-- <section class="admin-container">
-        <h1 class="admin-container-title">Toiminto: lisää uusi artikkeli tietokantaan</h1>
-        
+    <section class="admin-container">
+        <h1 class="admin-container-title">Toiminto: Vaihda pääkuvaa etusivulla</h1>
+        <div class="admin-container-explanation">
+            <p>Tässä osiossa voi vaihtaa pääkuvaa etusivulla.</p>
+            <p>Valitse kuvat vain <strong>JPG</strong>-muodossa:</p>
+            <ul class="first-level-list">
+                <li>kuvan leveys vähintään 1920px (jotta se näkyy selkeästi suurilla näytöillä),</li>
+                <li>kuvasuhde noin 16:10,</li>
+                <li>tiedostokoko enintään 2–3 Mt nopeaa latausta varten.</li>
+            </ul>
+            <p><strong>Poistettu pääkuva ei voi palauttaa!</strong></p>
+        </div>
+    </section>
 
-        <form method="post" id="admin-add-article" class="add-form" enctype="multipart/form-data" action="upload-to-database.php">
+    <section class="admin-container">
+        <form method="post" id="admin-change-main-img" class="add-form" enctype="multipart/form-data" action="main-img-manipulation.php">
+            <!-- Block with demonstration of actual HERO-image  -->
+            <div class="admin-hero-image">
+                <p>Tällä hetkellä pääkuva näyttää tältä:</p>
+                <img src="./images/main-banner.jpg" alt="Actual Hero image" width="200px">
+
+            </div>
+            <!-- Block to choose file -->
+            <div class="admin-add-upload-img">
+                <label class="upload-img-button-label" for="img-file">Valitse korvattava tiedosto:</label>
+                <input type="file" id="img-file" name="img-file" class="upload-img-button"  accept="image/*">
+                <p id="preview-img-text" hidden></p>
+                <img src="" alt="Uploaded hero image" width="200px" hidden id="preview-img">
+            </div>
+            <button type="submit" id="submit-btn" class="admin-btn">Lataa tietokantaan</button>
 
         </form>
+    </section>
 
-    </section> -->
-
-    <script src="./js_php/admin-menu-read-message.js" type="module"></script>
+    <script src="./js_php/admin-menu-change-hero-img.js" type="module"></script>
 
 </body>
 
