831b207600b6951edd76a9fac130bc7db961194f
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-10-04 11:24:34 +0300

    Remove exposed Google Maps API key

--- index.php
+++ index.php
@@ -180,7 +180,7 @@
       d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
     })
     ({
-      key: "REMOVED_KEY",
+      key: "",
       v: "weekly"
     });
   </script>
